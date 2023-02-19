a4b51841470c4123a72b0332e3d882aa81d04874
XiaoyongNI <87475986+KalmanNet@users.noreply.github.com>
2023-02-19 11:43:44 +0100

    update dataset for lorenz case

--- main_lor_decimation.py
+++ main_lor_decimation.py
@@ -34,8 +34,8 @@ print("Current Time =", strTime)
 ###################
 args = config.general_settings()
 ### dataset parameters
-args.N_E = 100
-args.N_CV = 5
+args.N_E = 1000
+args.N_CV = 10
 args.N_T = 10
 args.T = 3000
 args.T_test = 3000
@@ -48,8 +48,8 @@ args.wd = 1e-4
 offset = 0 # offset for the data
 chop = False # whether to chop the dataset sequences into smaller ones
 path_results = 'KNet/'
-DatafolderName = 'Simulations/Lorenz_Atractor/data/decimation/'
-DatafileName = 'decimated_r0_Ttest3000.pt'
+DatafolderName = 'Simulations/Lorenz_Atractor/data/'
+DatafileName = 'decimated_r0_Ttest3000_NT10.pt'
 data_gen = 'data_gen.pt'
 data_gen_file = torch.load(DatafolderName+data_gen)
 [true_sequence] = data_gen_file['All Data']
@@ -105,30 +105,6 @@ print("testset size:",test_target.size())
 print("trainset size:",train_target.size())
 print("cvset size:",cv_target_long.size())
 
-###############################
-### Load data from GNN-BP's ###
-###############################
-# compact_path = "Simulations/Lorenz_Atractor/data/lorenz_trainset300k.pickle"
-# with open(compact_path, 'rb') as f:
-#    data = pickle.load(f)
-# testdata = [data[0][0:T_test], data[1][0:T_test]]
-# states, meas = testdata
-# test_target =  torch.from_numpy(np.asarray(states, dtype=np.float32).transpose(1,0)).unsqueeze(0)
-# test_input = torch.from_numpy(np.asarray(meas, dtype=np.float32).transpose(1,0)).unsqueeze(0)
-# print("testset size:",test_target.size())
-# traindata = [data[0][T_test:(T_test+T*N_E)], data[1][T_test:(T_test+T*N_E)]]
-# states, meas = traindata
-# train_target =  torch.from_numpy(np.asarray(states, dtype=np.float32).transpose(1,0)).unsqueeze(0)
-# train_input = torch.from_numpy(np.asarray(meas, dtype=np.float32).transpose(1,0)).unsqueeze(0)
-# [train_target, train_input, train_init] = Short_Traj_Split(train_target, train_input, T)
-# cvdata = [data[0][(T_test+T*N_E):], data[1][(T_test+T*N_E):]]
-# states, meas = cvdata
-# cv_target_long =  torch.from_numpy(np.asarray(states, dtype=np.float32).transpose(1,0)).unsqueeze(0)
-# cv_input_long = torch.from_numpy(np.asarray(meas, dtype=np.float32).transpose(1,0)).unsqueeze(0)
-# [cv_target_long, cv_input_long, cv_init] = Short_Traj_Split(cv_target_long, cv_input_long, T)
-# print("trainset size:",train_target.size())
-# print("cvset size:",cv_target_long.size())
-
 ########################################
 ### Evaluate Observation Noise Floor ###
 ########################################
@@ -168,11 +144,11 @@ print("Observation Noise Floor(train dataset) - STD:", obs_std_dB, "[dB]")
 ########################
 ### Evaluate Filters ###
 ########################
-# ### EKF
-# print("Start EKF test J=5")
-# [MSE_EKF_linear_arr, MSE_EKF_linear_avg, MSE_EKF_dB_avg, EKF_KG_array, EKF_out] = EKF_test.EKFTest(args, sys_model_true, test_input, test_target)
-# print("Start EKF test J=2")
-# [MSE_EKF_linear_arr_partial, MSE_EKF_linear_avg_partial, MSE_EKF_dB_avg_partial, EKF_KG_array_partial, EKF_out_partial] = EKF_test.EKFTest(args, sys_model, test_input, test_target)
+### EKF
+print("Start EKF test J=5")
+[MSE_EKF_linear_arr, MSE_EKF_linear_avg, MSE_EKF_dB_avg, EKF_KG_array, EKF_out] = EKF_test.EKFTest(args, sys_model_true, test_input, test_target)
+print("Start EKF test J=2")
+[MSE_EKF_linear_arr_partial, MSE_EKF_linear_avg_partial, MSE_EKF_dB_avg_partial, EKF_KG_array_partial, EKF_out_partial] = EKF_test.EKFTest(args, sys_model, test_input, test_target)
 
 ########################################
 ### KalmanNet with model mismatch ######
@@ -187,32 +163,33 @@ print("Number of trainable parameters for KNet:",sum(p.numel() for p in KNet_mod
 # Train Neural Network
 KNet_Pipeline.setTrainingParams(args)
 if(chop):
-   KNet_Pipeline.NNTrain(args.N_E, train_input, train_target, args.N_CV, cv_input_long, cv_target_long,randomInit=True,train_init=train_init)
+   KNet_Pipeline.NNTrain(sys_model,cv_input_long,cv_target_long,train_input,train_target,path_results,\
+                         randomInit=True,train_init=train_init)
 else:
-   KNet_Pipeline.NNTrain(args.N_E, train_input, train_target, args.N_CV, cv_input_long, cv_target_long)
+   KNet_Pipeline.NNTrain(sys_model,cv_input_long,cv_target_long,train_input,train_target,path_results)
 # Test Neural Network
-[MSE_test_linear_arr, MSE_test_linear_avg, MSE_test_dB_avg, knet_out] = KNet_Pipeline.NNTest(args.N_T, test_input, test_target)
+[MSE_test_linear_arr, MSE_test_linear_avg, MSE_test_dB_avg, knet_out,t] = KNet_Pipeline.NNTest(sys_model,test_input,test_target,path_results)
 
 
 
 # Save trajectories
-trajfolderName = 'KNet/checkpoints/LorenzAttracotor/decimation/traj' + '/'
+trajfolderName = 'Simulations/Lorenz_Atractor' + '/'
 DataResultName = 'traj_lor_dec.pt'
 target_sample = torch.reshape(test_target[0,:,:],[1,m,args.T_test])
 input_sample = torch.reshape(test_input[0,:,:],[1,n,args.T_test])
 torch.save({           
-            # 'True':target_sample,
-            # 'Observation':input_sample,
-            # 'EKF J=5':EKF_out,
-            # 'EKF J=2':EKF_out_partial,                       
+            'True':target_sample,
+            'Observation':input_sample,
+            'EKF J=5':EKF_out,
+            'EKF J=2':EKF_out_partial,                       
             'KNet': knet_out,
             }, trajfolderName+DataResultName)
 
 #############
 ### Plot  ###
 #############
-titles = ["True Trajectory","Observation","KNet"]
-input = [target_sample,input_sample,knet_out]
+titles = ["True Trajectory","Observation","EKF","KNet"]
+input = [target_sample,input_sample,EKF_out_partial, knet_out]
 Net_Plot = Plot(trajfolderName,DataResultName)
 Net_Plot.plotTrajectories(input,3, titles,trajfolderName+"lor_dec_trajs.png")
 
